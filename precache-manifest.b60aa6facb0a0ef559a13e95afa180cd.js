f5f1a1bdca8742db566ee6e02f6c777fef797291
kensayers <kensayers@schillingschool.org>
2019-12-25 09:13:27 -0700

    Updates

---
+++ precache-manifest.b60aa6facb0a0ef559a13e95afa180cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ee6a2fc3992c28d35c75358acfd012",
+    "url": "/hx-hub/index.html"
+  },
+  {
+    "revision": "f6df7ff33ba34deb8518",
+    "url": "/hx-hub/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "a0f3c6571fbd6231cc64",
+    "url": "/hx-hub/static/css/main.0a3eb81e.chunk.css"
+  },
+  {
+    "revision": "f6df7ff33ba34deb8518",
+    "url": "/hx-hub/static/js/2.f49c299d.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/hx-hub/static/js/2.f49c299d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0f3c6571fbd6231cc64",
+    "url": "/hx-hub/static/js/main.d6a31126.chunk.js"
+  },
+  {
+    "revision": "810daef5f33e1ceb9a7c",
+    "url": "/hx-hub/static/js/runtime-main.878da889.js"
+  }
+]);
