e50e96eb5d749ce31be85d6b698f0101e73f7958
kensayers <kensayers@schillingschool.org>
2019-12-29 16:27:49 -0700

    Updates

---
+++ precache-manifest.ad52c4e430ff0e38906e701525f9e41e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b0aea2d3099c2b3848ed92b1fe5493",
+    "url": "/hx-hub/index.html"
+  },
+  {
+    "revision": "de06a155e28392af07cc",
+    "url": "/hx-hub/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "ac12d142f5c5da65f04e",
+    "url": "/hx-hub/static/css/main.0d01441c.chunk.css"
+  },
+  {
+    "revision": "de06a155e28392af07cc",
+    "url": "/hx-hub/static/js/2.f7ec1f3c.chunk.js"
+  },
+  {
+    "revision": "ab461327afd82fdab42048d6ea93652e",
+    "url": "/hx-hub/static/js/2.f7ec1f3c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac12d142f5c5da65f04e",
+    "url": "/hx-hub/static/js/main.d82c35f4.chunk.js"
+  },
+  {
+    "revision": "d3d7fce15e26eafb9995add41ed42346",
+    "url": "/hx-hub/static/js/main.d82c35f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "810daef5f33e1ceb9a7c",
+    "url": "/hx-hub/static/js/runtime-main.878da889.js"
+  },
+  {
+    "revision": "3506dc33e960118674cc3e662fdffe9b",
+    "url": "/hx-hub/static/media/FX_HX_Category_Impulse Response.3506dc33.png"
+  },
+  {
+    "revision": "c627940d4c440140fb52b55a9d6a49a5",
+    "url": "/hx-hub/static/media/FX_HX_Category_Merge.c627940d.png"
+  },
+  {
+    "revision": "6a016d8d8e3028932fac2e2932fbfae9",
+    "url": "/hx-hub/static/media/FX_HX_Category_Split.6a016d8d.png"
+  }
+]);
